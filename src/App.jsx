c85b159bf0b256742cfc78a1dfc17884b4eb9831
Harshit Pandey <hptech017@Harshits-MacBook-Air.local>
2025-03-10 13:33:23 +0530

    added responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import ProductSection from "./components/ProductSection";
 import SideBar1 from "./components/SideBar1";
 import Product from "./components/Product";
 
-// import "./styles/header.css";
-// import "./styles/sidebar.css";
-// import "./styles/hero.css";
-// import "./styles/product.css";
+
 
 function App() {
   return (
@@ -17,12 +13,7 @@ function App() {
       <Hero />
       <SideBar1 />
       <Product/>
-      { /*
-
-  
-      <ProductSection />
-      <ProductSection />
-      <ProductSection /> */}
+     
     </>
    
   );
